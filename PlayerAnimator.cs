7d25fc6204cd83393ddf2330ba7f90ab49b2fa9c
bibekchy456-cyber <bibekchy456@gmail.com>
2026-04-27 11:27:56 +0900

    Create PlayerAnimator.cs

---
+++ PlayerAnimator.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private const string IS_RUNNING = "IsRunning";
+
+    [SerializeField] private Player player;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        animator.SetBool(IS_RUNNING, player.IsRunning());
+    }
+}
